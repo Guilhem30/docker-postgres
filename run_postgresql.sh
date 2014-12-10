83bd666abb1281cf072fa0517ef7aa1eed44ba95
Guilhem Berna <guilhem.berna@gmail.com>
2014-12-10 09:22:45 +0000

    wait 2 seconds before creating the root user

--- run_postgresql.sh
+++ run_postgresql.sh
@@ -2,7 +2,7 @@
 set -e
 
 su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf &"
-
+sleep 2
 if [ -f /firstrun ]
 then
         rm /firstrun
